c599c5c908ef4c4c718a10891ce84e2c1ff5ccc5
Alin Grigorescu <alin.grig87@gmail.com>
2023-12-23 12:50:15 +0200

    add: scroll to an element on the page

--- register-form/src/test/java/tests/GoogleSearchTest.java
+++ register-form/src/test/java/tests/GoogleSearchTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -31,5 +32,8 @@ public class GoogleSearchTest {
 
         WebElement googleAtelierulDigitalLink = driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div[1]/div/div/span/a/h3"));
         googleAtelierulDigitalLink.click();
+
+        WebElement aflaMaiMulteLink = driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/div[3]/div[2]/a/span"));
+        new Actions(driver).scrollToElement(aflaMaiMulteLink).perform();
     }
 }
