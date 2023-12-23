ead61086366ffee6038c522604b5e5c9df56c30a
Alin Grigorescu <alin.grig87@gmail.com>
2023-12-23 12:45:13 +0200

    add: search on google, open first link

--- register-form/src/test/java/tests/GoogleSearchTest.java
+++ register-form/src/test/java/tests/GoogleSearchTest.java
@@ -1,7 +1,10 @@
 package tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -18,6 +21,15 @@ public class GoogleSearchTest {
     public void SearchOnGoogle() {
         driver = new ChromeDriver();
         driver.get("https://google.com");
-    }
 
+        WebElement acceptCookiesButton = driver.findElement(By.id("L2AGLb"));
+        acceptCookiesButton.click();
+
+        WebElement searchInput = driver.findElement(By.name("q"));
+        searchInput.sendKeys("Google Atelierul Digital", Keys.ENTER);
+
+
+        WebElement googleAtelierulDigitalLink = driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div[1]/div/div/span/a/h3"));
+        googleAtelierulDigitalLink.click();
+    }
 }
